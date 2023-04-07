8e246fe359f74449529cb63c8b5cd1898a2bad56
vinh nguyễn <vinhntse2002@gmail.com>
2023-04-07 14:17:27 +0700

    New

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+
+// creat Component App
+
+function App(){
+    return(
+        <div>
+            <h1>Xin chào anh em F8!</h1>
+        </div>
+    )
+}
+
+
+// render
+ReactDOM.render(<App />, document.getElementById('root'));
